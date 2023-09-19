00efe1541375cc7f5a4abe58764c54c7e71ac1f0
Yareli Zavala <128005413+yareeli21@users.noreply.github.com>
2023-09-19 00:13:31 -0600

    Add files via upload

---
+++ p8.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+//programa que dada una matriz de tipo flotante, determinar: si es nula, obtner K*R y R/K y si es una matriz triangular superior
+int **crearMatriz(int fil,int col);
+void darDatos(int **mtz,int fil,int col);
+void mostrarM(int **mtz,int fil,int col);
+void liberarM(int **mtz,int numElem);
+void Mensajes(int msg);
+int esNula(int **mtz,int fil,int col);
+int
+
+void main(){
+
+
+
+}
